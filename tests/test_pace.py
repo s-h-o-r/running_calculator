b1ff1b8b42274665643923b1c6204c009785f93e
Daniil Bugaev <bugaev.daniil.a@gmail.com>
2026-03-01 01:22:46 +0300

    fixed Tempo.fromMinutes() for working with str + little refactoring for Speed class

--- tests/test_pace.py
+++ tests/test_pace.py
@@ -8,11 +8,11 @@ data = [
     (2.5, '2:30', 150, 2, 30),
     (5.2, '5:12', 312, 5, 12),
     (5.2, '5:12', 312, 5, 12),
-    (7.75, '7:48', 468, 7, 48),
+    (7.75, '7:45', 465, 7, 45),
     ('10', '10:00', 600, 10, 0),
 ]
 
-@pytest.mark.parametrize("minutes,expected_str,expected_seconds,expected_minutes,expected_only_seconds", data)
+@pytest.mark.parametrize('minutes,expected_str,expected_seconds,expected_minutes,expected_only_seconds', data)
 def test_from_minutes(minutes, expected_str, expected_seconds, expected_minutes, expected_only_seconds):
     tempo = Tempo.fromMinutes(minutes)
     assert(tempo.getInSeconds() == expected_seconds)
@@ -21,10 +21,21 @@ def test_from_minutes(minutes, expected_str, expected_seconds, expected_minutes,
     assert(tempo.str() == expected_str)
 
 
-@pytest.mark.parametrize("_,minutes_str,expected_seconds,expected_minutes,expected_only_seconds", data)
+@pytest.mark.parametrize('_,minutes_str,expected_seconds,expected_minutes,expected_only_seconds', data)
 def test_from_str(_, minutes_str, expected_seconds, expected_minutes, expected_only_seconds):
     tempo = Tempo.fromStrPace(minutes_str)
     assert(tempo.getInSeconds() == expected_seconds)
     assert(tempo.getOnlyMinutes() == expected_minutes)
     assert(tempo.getOnlySeconds() == expected_only_seconds)
     assert(tempo.str() == minutes_str)
+
+error_str = [
+    ('random str'),
+    ('10.'),
+    ('10.1243124')
+]
+
+@pytest.mark.parametrize('error_str', error_str)
+def test_convert_non_int_str_to_tempo(error_str):
+    with pytest.raises(ValueError):
+        Tempo.fromMinutes(error_str)
