147d26cb2e1b4cc0573f5b71d69dc09f159e144b
Daniil Bugaev <bugaev.daniil.a@gmail.com>
2026-03-02 01:58:28 +0300

    first touch of telegram bot api

---
+++ src/tg_bot/main.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot('')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, 'Fuck yeah')
+
+@bot.message_handler(func=lambda message: True)
+def message_handler(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
