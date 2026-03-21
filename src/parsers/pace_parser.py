d9832dae1f3d94ada96cc803c91c5f0a5280c970
Daniil Bugaev <bugaev.daniil.a@gmail.com>
2026-03-22 01:14:26 +0300

    add pace parsing and test for ParsedInput

--- src/parsers/pace_parser.py
+++ src/parsers/pace_parser.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from typing import Optional, Union
-from domain.pace import Speed, Tempo
+from domain.pace import Speed, Tempo, SECONDS_IN_MINUTE
 
 @dataclass(frozen=True)
 class ParsedInput:
@@ -10,6 +10,45 @@ class ParsedInput:
     def is_valid(self) -> bool:
         return self.error is None and self.value is not None
 
+@dataclass(frozen=True)
+class ParsedPace:
+    minutes: int = None
+    seconds: int = None
 
-def parse_speed_or_tempo(raw: str) -> ParsedInput:
-    pass
+    def is_valid(self):
+        return (self.minutes is not None 
+                and self.seconds is not None 
+                and self.seconds <= 60)
+
+
+def parse_pace(raw_pace: str, delim_pos: int = None) -> ParsedPace:
+    if(raw_pace is None):
+        return ParsedPace()
+    
+    delim_pos = delim_pos if delim_pos is None else raw_pace.find(':')
+    if(delim_pos == -1):
+        return ParsedPace()
+
+    left_part = raw_pace[:delim_pos]
+    right_part = raw_pace[delim_pos + 1:]
+    minutes = int(left_part) if left_part.isdecimal() else None
+    seconds = int(right_part) if right_part.isdecimal() else None
+
+    return ParsedPace(minutes, seconds)
+
+def parse_speed_or_tempo(raw: str = '') -> ParsedInput:
+    raw = raw.strip() if raw is not None else ''
+    if(raw == ''):
+        return ParsedInput(error='Input is empty')
+
+    delim_pos = raw.find(':')
+    if(delim_pos != -1):
+        parsed_pace: ParsedPace = parse_pace(raw, delim_pos)
+
+        if(parsed_pace.is_valid()):
+            return ParsedInput(value=Tempo(parsed_pace.minutes * SECONDS_IN_MINUTE + parsed_pace.seconds))
+        else:
+            return ParsedInput(error='Input looks like pace but it\'s incorrect')
+
+
+    return ParsedInput(value=Speed(raw_value=10.0))
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,7 @@
 import pytest
 
 from domain.pace import Speed, Tempo
-from parsers.pace_parser import ParsedInput
+from parsers.pace_parser import ParsedInput, parse_speed_or_tempo
 
 @pytest.mark.parametrize(
     'value, error, expected_result',
@@ -16,3 +16,22 @@ from parsers.pace_parser import ParsedInput
 def test_is_valid_input(value, error, expected_result):
     input = ParsedInput(value=value, error=error)
     assert input.is_valid() == expected_result
+
+# In this test error message doesn't matter
+@pytest.mark.parametrize(
+    'raw_input, expected_parsed_input',
+    [
+        (None, ParsedInput(value=None, error='')),
+        ('', ParsedInput(value=None, error='')),
+        ('10', ParsedInput(value=Speed(raw_value=10.0), error=None)),
+        ('  ', ParsedInput(value=None, error='')),
+        ('10:00', ParsedInput(value=Tempo(seconds=600), error=None)),
+        (' 5:47 ', ParsedInput(value=Tempo(seconds=347), error=None)),
+        ('5:61', ParsedInput(value=None, error=''))
+    ]
+)
+def test_parse_speed_or_tempo(raw_input, expected_parsed_input):
+    parsed_input = parse_speed_or_tempo(raw_input)
+    assert parsed_input.is_valid() == expected_parsed_input.is_valid()
+    assert parsed_input.value == expected_parsed_input.value
+    assert type(parsed_input.error) == type(expected_parsed_input.error)
